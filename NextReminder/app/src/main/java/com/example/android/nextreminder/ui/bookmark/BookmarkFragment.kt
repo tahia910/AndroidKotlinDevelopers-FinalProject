79f45717a04063955b02891e2f1ad08ef52073fb
Tahia <37906654+tahia910@users.noreply.github.com>
2022-06-18 18:55:09 +0900

    add app base

---
+++ NextReminder/app/src/main/java/com/example/android/nextreminder/ui/bookmark/BookmarkFragment.kt
@@ -0,0 +1,41 @@
+package com.example.android.nextreminder.ui.bookmark
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.nextreminder.databinding.FragmentBookmarkBinding
+
+class BookmarkFragment : Fragment() {
+
+    private var _binding: FragmentBookmarkBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val bookmarkViewModel = ViewModelProvider(this)[BookmarkViewModel::class.java]
+
+        _binding = FragmentBookmarkBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        bookmarkViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
